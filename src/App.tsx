301b9b1dbd833f3cb6ac8325368e2fd16f538f76
Nikita <nikitka.crasnov2013@mail.ru>
2022-05-12 21:15:02 +0300

    Add bx24

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,17 @@ import { Auth } from "./pages/Auth/Auth";
 import { useSelector } from "react-redux";
 import { RootState } from "./redux/configureStore";
 import { ProtectedRoute } from "./pages/ProtectedRoute/ProtectedRoute";
+import { BX24 } from 'bx24';
 
 function App() {
   const { data } = useSelector((state: RootState) => state.user);
+
+  // eslint-disable-next-line no-restricted-globals
+  const bx24 = new BX24(window, parent);
+
+  bx24.getAuth().then(function(auth) {
+    console.log(auth);
+  });
 
   return data ? <ProtectedRoute /> : <Auth />;
 }
