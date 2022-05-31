cd797f23f834271df8f1566a2096d9539720367c
Nikita <nikitka.crasnov2013@mail.ru>
2022-05-31 22:49:10 +0300

    refresh-bot

--- netlify/functions/deploy-succeeded/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded/deploy-succeeded.js
@@ -1,8 +1,11 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const fetch = require('node-fetch')
+
 const handler = async (event) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
     console.log(JSON.parse(event.body))
+    fetch(`https://api.telegram.org/bot5417214204:AAH7YOhkgmeScFPWc5cSQOPE29dxi8VAa2k/sendMessage?chat_id=@edisondeploylog&text=%F0%9F%91%B7%20Deploy%20Preview%20for%20wondrous-nasturtium-d39cd2%20processing.%20%F0%9F%91%B7%20Deploy%20Preview%20for%20wondrous-nasturtium-d39cd2%20processing.`)
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject}` }),
