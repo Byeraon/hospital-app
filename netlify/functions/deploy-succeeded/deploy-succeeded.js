82698961fb5d0eb60103119988f109e40af6e4ba
Nikita <nikitka.crasnov2013@mail.ru>
2022-05-31 23:36:31 +0300

    new

--- netlify/functions/deploy-succeeded/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded/deploy-succeeded.js
@@ -6,7 +6,7 @@ exports.handler = async (event, context) => {
   console.log(JSON.parse(event.body))
   let response
   try {
-    response = await fetch(`https://api.telegram.org/bot5417214204:AAH7YOhkgmeScFPWc5cSQOPE29dxi8VAa2k/sendMessage?chat_id=@edisondeploylog&text=🔨 Latest commit - ${JSON.parse(event.body).payload.commit_ref} %0A😎 Deploy Preview - ${JSON.parse(event.body).payload.links.permalink} %0A👁 Committer - ${JSON.parse(event.body).payload.committer}`)
+    response = await fetch(`https://api.telegram.org/bot5417214204:AAH7YOhkgmeScFPWc5cSQOPE29dxi8VAa2k/sendMessage?chat_id=@edisondeploylog&text=🖥 Project - ${JSON.parse(event.body).payload.name} %0A🔨 Latest commit - ${JSON.parse(event.body).payload.commit_ref} %0A😎 Deploy Preview - ${JSON.parse(event.body).payload.links.permalink} %0A👁 Committer - ${JSON.parse(event.body).payload.committer}`)
     // handle response
   } catch (err) {
     console.log("DIE")
