cf7575d71ee999e2a733d97958212f1eddca1586
Nikita <nikitka.crasnov2013@mail.ru>
2022-05-31 22:55:24 +0300

    change hook

--- netlify/functions/deploy-succeeded/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded/deploy-succeeded.js
@@ -1,21 +1,27 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const fetch = require('node-fetch')
 
-const handler = async (event) => {
+const API_ENDPOINT = 'https://api.telegram.org/bot5417214204:AAH7YOhkgmeScFPWc5cSQOPE29dxi8VAa2k/sendMessage?chat_id=@edisondeploylog&text=%F0%9F%91%B7%20Deploy%20Preview%20for%20wondrous-nasturtium-d39cd2%20processing.%20%F0%9F%91%B7%20Deploy%20Preview%20for%20wondrous-nasturtium-d39cd2%20processing.'
+
+exports.handler = async (event, context) => {
+  let response
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    console.log(JSON.parse(event.body))
-    fetch(`https://api.telegram.org/bot5417214204:AAH7YOhkgmeScFPWc5cSQOPE29dxi8VAa2k/sendMessage?chat_id=@edisondeploylog&text=%F0%9F%91%B7%20Deploy%20Preview%20for%20wondrous-nasturtium-d39cd2%20processing.%20%F0%9F%91%B7%20Deploy%20Preview%20for%20wondrous-nasturtium-d39cd2%20processing.`)
+    response = await fetch(API_ENDPOINT)
+    console.log(response)
+    // handle response
+  } catch (err) {
     return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response
+    })
   }
 }
-
-module.exports = { handler }
